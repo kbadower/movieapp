839ae9049c120c72263bb19f9af9cfeda9448841
kbadower <viader258>
2019-04-14 14:58:31 +0200

    Added service

--- src/main/java/com/badower/movieapp/Movie.java
+++ src/main/java/com/badower/movieapp/Movie.java
@@ -3,6 +3,10 @@ package com.badower.movieapp;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Objects;
 
 @Entity
 public class Movie {
@@ -10,7 +14,11 @@ public class Movie {
     @Id
     @GeneratedValue
     private Long id;
+    @NotNull
+    @Min(1900)
     private Integer year;
+    @NotNull
+    @Size(min=1, max=30)
     private String title;
 
     public Long getId() {
@@ -35,5 +43,20 @@ public class Movie {
 
     public void setTitle(String title) {
         this.title = title;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Movie movie = (Movie) o;
+        return Objects.equals(id, movie.id) &&
+                Objects.equals(year, movie.year) &&
+                Objects.equals(title, movie.title);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, year, title);
     }
 }
--- src/test/java/com/badower/movieapp/MovieControllerTest.java
+++ src/test/java/com/badower/movieapp/MovieControllerTest.java
@@ -10,8 +10,14 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -32,7 +38,7 @@ public class MovieControllerTest {
     }
 
     @Test
-    public void should_get_empty_movies() throws Exception{
+    public void should_get_empty_movies() throws Exception {
         //when(service.greet()).thenReturn("Hello Mock");
         this.mockMvc.perform(get("/movies"))
                 .andDo(print())
@@ -42,20 +48,109 @@ public class MovieControllerTest {
     }
 
     @Test
-    public void should_add_entity() throws Exception{
-        //when(service.greet()).thenReturn("Hello Mock");
+    public void should_add_entity() throws Exception {
+        Movie expectedMovie = new Movie();
+        expectedMovie.setId(1L);
+        expectedMovie.setYear(2005);
+        expectedMovie.setTitle("TestTitle");
+
+
         this.mockMvc.perform(post("/movies")
                 .contentType(MediaType.APPLICATION_JSON)
                 .content("{\"id\": 1, \"year\": 2005, \"title\": \"TestTitle\"}"))
                 .andDo(print())
                 .andExpect(status().isOk());
 
+        verify(movieRepository).save(expectedMovie);
+
+
+    }
+
+    @Test
+    public void should_get_entity() throws Exception {
+
+        Movie expectedMovie = new Movie();
+        expectedMovie.setId(1L);
+        expectedMovie.setYear(2005);
+        expectedMovie.setTitle("TestTitle");
+
+        when(movieRepository.findById(1L)).thenReturn(Optional.of(expectedMovie));
+
         this.mockMvc.perform(get("/movies/1"))
                 .andDo(print())
                 .andExpect(status().isOk())
                 .andExpect(content().json("{\"id\": 1, \"year\": 2005, \"title\": \"TestTitle\"}"))
                 .andExpect(content().contentTypeCompatibleWith(MediaType.APPLICATION_JSON));
+    }
 
+    @Test
+    public void should_update_entity() throws Exception{
+        Movie expectedMovie = new Movie();
+        expectedMovie.setId(1L);
+        expectedMovie.setYear(2005);
+        expectedMovie.setTitle("TestTitle");
+
+        Movie expectedMovie2 = new Movie();
+        expectedMovie.setId(1L);
+        expectedMovie.setYear(2006);
+        expectedMovie.setTitle("Title2");
+
+        when(movieRepository.findById(1L)).thenReturn(Optional.of(expectedMovie));
+
+        this.mockMvc.perform(put("/movies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\": 1, \"year\": 2006, \"title\": \"Title2\"}"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+
+
+        verify(movieRepository).findById(1L);
+        verify(movieRepository).save(expectedMovie2);
+    }
+
+    @Test
+    public void should_put_new_entity() throws Exception{
+        Movie expectedMovie = new Movie();
+        expectedMovie.setId(1L);
+        expectedMovie.setYear(2005);
+        expectedMovie.setTitle("TestTitle");
+
+        when(movieRepository.findById(1L)).thenReturn(Optional.empty());
+
+        this.mockMvc.perform(put("/movies")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content("{\"id\": 1, \"year\": 2005, \"title\": \"TestTitle\"}"))
+                .andDo(print())
+                .andExpect(status().isOk());
+
+        verify(movieRepository).findById(1L);
+        verify(movieRepository).save(expectedMovie);
 
     }
+
+    @Test
+    public void should_delete_entity() throws Exception{
+        Movie expectedMovie = new Movie();
+        expectedMovie.setId(1L);
+        expectedMovie.setYear(2005);
+        expectedMovie.setTitle("TestTitle");
+
+        when(movieRepository.findById(1L)).thenReturn(Optional.of(expectedMovie));
+
+        this.mockMvc.perform(delete("/movies/1"))
+                .andDo(print())
+                .andExpect(status().isNoContent());
+
+        verify(movieRepository).deleteById(1L);
+    }
+
+    @Test
+    public void should_not_pass_year_validation() throws Exception {
+
+
+        verify(movieRepository, never()).save(any());
+    }
+
+
 }
